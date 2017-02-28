9b02a1dea9f7272baca2798ec6c1d0a1639a6149
BaptisteBruneau <baptiste.bruneau@gmail.com>
2017-02-28 17:27:56 +0100

    Revert "Revert "Initilisation du projet""
    This reverts commit 10c097beeadc9a48ac4ec458018e3e335fa4bc0f.

---
+++ TutoCodeProject/DataAccessLayer/SalesERPDAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TutoCodeProject.Models;
+namespace TutoCodeProject.DataAccessLayer
+{
+    public class SalesERPDAL : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public SalesERPDAL() : base("Data Source=(local); Initial Catalog=SalesERPDB; Integrated Security=True")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Créer une table selon la classe Employé. 
+            modelBuilder.Entity<Employee>().ToTable("TblEmployee");
+            base.OnModelCreating(modelBuilder);
+
+            //INFO: La relation avec la connection string (web.config) se fait grâce à la propriété name de cette dernière. Celle-ci doit avoir
+            //le même nom que la classe.
+        }
+    }
+}
